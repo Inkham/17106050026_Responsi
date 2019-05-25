dfde6529324b306fd62ff2d0d07165d7a44080fb
Inkham <47770253+Inkham@users.noreply.github.com>
2019-05-25 23:45:55 +0700

    Add files via upload

---
+++ database/migrations/2019_05_20_131733_create_book_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->integer('bookId')->autoIncrement();
+            $table->string('bookTitle', 100);
+            $table->integer('penulisId');
+            $table->integer('quantity');
+            $table->string('image_url');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+}
---
+++ database/migrations/2019_05_20_132527_create_penulis_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePenulisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penulis', function (Blueprint $table) {
+            $table->integer('penulisId')->autoIncrement();
+            $table->string('penulisName', 100);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penulis');
+    }
+}
